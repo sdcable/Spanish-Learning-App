e6924ddbb1972c3d8689bb4dfe8cefaa7187ac9a
Spencer <sdcable@byu.edu>
2024-10-10 08:57:59 -0600

    Initial Commit

---
+++ LearnSpanish/LearnSpanishApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSpanishApp.swift
+//  LearnSpanish
+//
+//  Created by IS 543 on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnSpanishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
